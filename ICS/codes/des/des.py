882d946e895d3a486f40dc309da53eec291aaae5
Guiselle Tatiana Zambrano Penagos <gtzambranop@unal.edu.co>
2020-10-20 14:07:48 -0500

    des other version

--- ICS/codes/des/des.py
+++ ICS/codes/des/des.py
@@ -1,14 +1,8 @@
 import base64
 from pyDes import *
 
-def use_base64(img, encode):
-    if encode:
-        temp = base64.b64encode(img)
-        result = temp.decode('utf-8')
-    else:
-        temp = img.encode('utf-8')
-        result = base64.decodebytes(temp)
-    return result
+def use_base64():
+    pass
 
 def open_file(name_file):
     with open(name_file, 'rb') as binary_file:
@@ -20,7 +14,7 @@ def encrypt_img(img, key):
     encrypted_img = new_des.encrypt(str(img))
     return base64.b64encode(encrypt_img)
 
-def decrypt_img(encrypt, key)
+def decrypt_img(encrypt, key):
     new_des = des(key, CBC, '\0\0\0\0\0\0\0\0', padmode=PAD_PKCS5)
     img = base64.b64decode(encrypt)
     new_img = new_des.decrypt(img, padmode=PAD_PKCS5)
@@ -32,6 +26,25 @@ def main():
     img_name = "input.png"
     mode = 0
     img = open_file(img_name)
+    new_des = des(key, CBC, '\0\0\0\0\0\0\0\0', padmode=PAD_PKCS5)
+    encrypted_img = new_des.encrypt(str(img))
+    enc = base64.b64encode(encrypted_img)
+    res = enc.decode('utf-8')
+
+
+    test = res.encode('utf-8')
+    data = base64.decodebytes(test)
+    img = new_des.decrypt(data, padmode=PAD_PKCS5)
+
+    with open('result.png', 'wb') as file_to_save:
+        file_to_save.write(img)
+    '''
+    if mode == 0:
+        encrypted_img = new_des.encrypt(str(img))
+        enc = base64.b64encode(encrypted_img)
+        print(enc)
+    else:
+    '''
 
 if __name__ == "__main__":
     main()
